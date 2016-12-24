77f259779833b9ebcdbb1072e73590d3bc6bc648
LuyaoLI <286299392@qq.com>
2016-12-24 14:10:07 +0800

    Empty Class

---
+++ LispInterpreter/Eval.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispInterpreter
+{
+    class Eval
+    {
+        public int eval(List<Object> parseTree, Closure closure) 
+        {
+            List<Object> list = new List<Object>();
+            for (int i = 0; i < parseTree.Count(); i++)
+            {
+                Object tmp = parseTree.ElementAt(i);
+                if (tmp.GetType().Equals(list.GetType())) 
+                {
+                    return 0;
+                }
+           
+            }
+
+            return 0;
+        }
+
+
+    }
+}

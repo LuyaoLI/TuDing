0d86e59ad74fa325ba126c7795a5e4b444d96d33
robinx <xingrobin@gmail.com>
2016-12-25 12:48:08 +0800

    Make Repl Great Again!

--- LispInterpreter/Tuding.cs
+++ LispInterpreter/Tuding.cs
@@ -5,16 +5,32 @@ namespace LispInterpreter
 {
 	public class Tuding
 	{
+		static void Repl(Env env) {
+			Console.WriteLine ("TuDing Lisp Interpreter REPL");
+			while (true) {
+				try {
+				Console.Write (">> ");
+				String exp = Console.ReadLine();
+				Parse parse = new Parse (exp);
+				Value value = Eval.evalProgaram (parse.parseTree, env);
+				Console.WriteLine (value.ToString ());
+				} catch (Exception e) {
+					Console.WriteLine ("Invalid Statement");
+				}
+			}
+		}
+
 		static void Main (String[] args)
 		{
+			Env env = new Env ();
+			string sys = File.ReadAllText ("System.td");
+			Eval.evalProgaram (new Parse (sys).parseTree, env);
 			if (args.Length < 1) {
-				Repl.Main (args);
+				Repl (env);
 			}
 			string path = args[0];
 			string code = File.ReadAllText (path);
-			Env env = new Env ();
 			Parse parse = new Parse (code);
-			parse.showParseTree (" ", parse.parseTree);
 			Value value = Eval.evalProgaram (parse.parseTree, env);
 			Console.WriteLine (value.ToString ());
 		}
